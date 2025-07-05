2314dd2b966f1d130ffb7cc718c5fe7ef660ba46
RCInet <contact.rci.net@gmail.com>
2025-07-05 22:15:16 +0200

    Update AssetBundle

--- AssetBundleExport/Assets/Scripts/Editor/CreateAssetBundles.cs.cs
+++ AssetBundleExport/Assets/Scripts/Editor/CreateAssetBundles.cs.cs
@@ -15,7 +15,16 @@ public class CreateAssetBundles
             BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
         }
         catch (Exception e) { Debug.LogWarning(e); }
-        
-		
+	}
+	
+public class CreateAssetBundles2
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles2()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if(!Directory.Exists(assetBundleDirectory)) { Directory.CreateDirectory(assetBundleDirectory); }
+		BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
     }
+}
 }
