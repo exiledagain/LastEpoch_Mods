e23be054751371e1897ccecd235706027430492c
RCInet <contact.rci.net@gmail.com>
2025-09-21 00:47:02 +0200

    Post build script
    Added script to create latest archives on build using Winrar

---
+++ LastEpoch_Hud/MakeLatest.bat
@@ -0,0 +1,35 @@
+@echo off
+SET "winrar=C:\Program Files\WinRAR\WinRAR.exe"
+SET "keyboard_file=LastEpoch_Hud(Keyboard).rar"
+SET "gamepad_file=LastEpoch_Hud(WinGamepad).rar"
+SET "unwanted_file=LastEpoch_Hud.deps.json"
+cd %~dp0
+cd ..\Build\Keyboard\net6.0\
+SET "keyboard_dir=%cd%
+cd %~dp0
+cd ..\Build\WinGamepad\net6.0\
+SET "gamepad_dir=%cd%
+IF EXIST %keyboard_dir%\%unwanted_file% (
+		del "%keyboard_dir%\%unwanted_file%"
+)
+IF EXIST %gamepad_dir%\%unwanted_file% (
+		del "%gamepad_dir%\%unwanted_file%"
+)
+cd %~dp0
+cd ..\Latest\
+SET "latest_dir=%cd%
+mkdir %latest_dir%
+IF EXIST %latest_dir%\%keyboard_file% (
+	del "%latest_dir%\%keyboard_file%"
+)
+IF EXIST %latest_dir%\%gamepad_file% (
+	del "%latest_dir%\%gamepad_file%"
+)
+IF EXIST %keyboard_dir%\* (
+	cd %keyboard_dir%
+	"%winrar%" a -r "%latest_dir%\%keyboard_file%"
+)
+IF EXIST %gamepad_dir%\* (
+	cd %gamepad_dir%
+	"%winrar%" a -r "%latest_dir%\%gamepad_file%"
+)
